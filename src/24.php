1ebc18068b28502cc0aa4990803a1c7917fd6bc3
celikcarsten77 <celikcarsten77@gmail.com>
2025-04-05 06:15:40 +0000

    Add 24.php

---
+++ src/24.php
@@ -0,0 +1,13 @@
+<?php
+// This is a simple PHP script to demonstrate basic usage of variables and if statements.
+// The variable $userAge is set to 30,
+// which represents the age of the user. If the user's age is less than or equal to 18,
+// then the message "Welcome" will be displayed, otherwise "Please wait." will be shown.
+
+$userAge = 30; // Set a variable with an initial value
+if ($userAge <= 18) {
+    echo "Welcome";
+} else {
+    echo "Please wait.";
+}
+?>
